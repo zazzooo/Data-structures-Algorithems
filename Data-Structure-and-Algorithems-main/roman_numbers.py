310b6109f044ef020740155f27360d137cbdadac
zazzooo <lorenzo.gini1998@gmail.com>
2021-09-28 14:41:00 +0200

    roman number finished

--- Data-Structure-and-Algorithems-main/roman_numbers.py
+++ Data-Structure-and-Algorithems-main/roman_numbers.py
@@ -2,14 +2,34 @@
 def roman_number(n):
     result = ''
     n_str = str(n)
+    if len(n_str) > 3:
+        # print(n_str[:-3])
+        result = int(n_str[:-3])*'M' + result
+    n_str = n_str[-3:]
+    print(n_str)
     for i in range(len(n_str)):
-        cifra = int(n_str[i])
-        print(cifra)
-        if cifra > 5:
-            result = result + 'V'
-            cifra -= 5
-        print(cifra)
-        result = result + cifra*'I'
+        # print(i,len(n_str))
+        if i == len(n_str)-1:
+            cifra = int(n_str[i])
+            if cifra >= 5:
+                result = result + 'V'
+                cifra -= 5
+            result = result + cifra*'I'
+        if i == len(n_str)-2:
+            cifra = int(n_str[i])
+            # print(cifra)
+            if cifra >= 5:
+                result = result + 'L'
+                cifra -= 5
+            result = result + cifra*'X'
+        if i == len(n_str)-3:
+            cifra = int(n_str[i])
+            if cifra >= 5:
+                result = result + 'D'
+                cifra -= 5
+            result = result + cifra*'C'
+        if i == len(n_str)-4:
+            break
     return result
 
-print(roman_number(45))
+print(roman_number(2736))
